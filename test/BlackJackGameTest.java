91d83a2de2218b0553477807bbf8b05376b93593
joelyuatgmaildotcom <96020237+joelyuatgmaildotcom@users.noreply.github.com>
2021-12-12 14:50:03 -0500

    Update BlackJackGameTest.java

--- test/BlackJackGameTest.java
+++ test/BlackJackGameTest.java
@@ -1,12 +1,25 @@
 package test;
 
 import org.junit.*;
+import app.*;
+
+import java.util.*;
+
 public class BlackJackGameTest {
-    
-    @Test
-    public void test() {
 
-        Assert.assertTrue(true);
+    private BlackJackGame game;
 
+    List<Player> players;
+
+    @Before
+    public void setup() {
+        players = new ArrayList<>();
+        players.add(new HumanPlayer("1"));
+        players.add(new HumanPlayer("2"));
+
+        game = new BlackJackGame(players,2);
     }
+    
+    //TODO How do i test code with standard IN?
+    //get library with Reflection Util to examine fields of tested class
 }
